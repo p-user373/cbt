d1de603fb6a798d4bd69cdc8cd7a45fc15a403c3
p-user373 <78620499+p-user373@users.noreply.github.com>
2021-06-05 14:27:34 +0900

    Add files via upload

---
+++ database/migrations/2021_05_21_184919_create_choices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger("q_id");
+            $table->tinyInteger("c_id");
+            $table->string("c_sentence");
+            $table->tinyInteger("correct_or_not");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('choices');
+    }
+}
